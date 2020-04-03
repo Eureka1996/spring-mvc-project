5e594007dc9735ebd97161b9baf75be592473ea7
Eureka1996 <1767778344@qq.com>
2020-04-03 20:54:47 +0800

    Spring MVC 自定义视力解析类

---
+++ spring-mvc-base/src/main/java/com/wufuqiang/springmvc/views/HelloView.java
@@ -0,0 +1,29 @@
+package com.wufuqiang.springmvc.views;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.View;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class HelloView implements View {
+	/**
+	 * 返回内容类型
+	 * @return
+	 */
+	@Override
+	public String getContentType() {
+		return "text/html";
+	}
+
+	@Override
+	public void render(Map<String, ?> map,
+	                   HttpServletRequest request,
+	                   HttpServletResponse response) throws Exception {
+		response.getWriter().println("hello view,time:"+new Date());
+
+	}
+}
